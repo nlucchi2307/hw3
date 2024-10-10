bf65e9fe535f9dc79cc8c17b806bdf144d699a0d
Marta Sala <marta.sala@bse.eu>
2024-10-10 17:07:37 +0100

    Added hw3.py completed

--- hw3.py
+++ hw3.py
@@ -71,3 +71,59 @@ def read_data_with_pandas(filename="data.csv"):
         return None
     except Exception as e:
         print(f"An error occurred: {e}")
+        
+# 5) Squash some bugs! 
+# Find the possible logical errors (bugs) 
+# in the code blocks below. Comment in each of them
+# which logical errors did you find and correct them
+### (a)
+total_double_sum = 0
+for elem in [10, 5, 2]:
+    double = elem * 2
+    total_double_sum += elem
+
+#This code is used to to add the doubled value of each element to total_double_sum. However, it is currently adding the original element (elem), not the doubled value.
+#The corrected version of the code block is the following:
+
+total_double_sum = 0
+for elem in [10, 5, 2]:
+    double = elem * 2
+    total_double_sum += double
+
+### (b)
+strings = ''
+for string in ['I', 'am', 'Groot']:
+    strings = string+"_"+string
+
+#The code will only assign the last iteration's value (Groot_Groot) to strings producing "Groot_Groot".Considering that the goal is to concatenate all strings with underscores (e.g., "I_am_Groot"), this logic needs to be fixed.
+#The corrected version of the code block is the following:
+
+strings = ''
+for string in ['I', 'am', 'Groot']:
+    strings += string + "_"
+strings = strings.rstrip('_') # This removes the trailing underscore
+
+
+### (c) Careful!
+j=10
+while j > 0:
+   j += 1
+
+#This loop will run forever because because j is being incremented (j += 1) every time the loop runs. Since j starts at 10 and keeps getting larger, it will never be less than or equal to zero, which means the condition j > 0 will always be true. As a result, the loop never stops.
+#The corrected version of the code block is the following:
+
+j=10
+while j > 0:
+   j -= 1
+
+### (d)
+productory = 0
+for elem in [1, 5, 25]:
+    productory *= elem
+
+#Initializing productory to 0 will result in the product always being zero since anything multiplied by zero remains zero. Instead should be initialized to 1.
+#The corrected version of the code block is the following:
+
+productory = 1
+for elem in [1, 5, 25]:
+    productory *= elem
